c4164e2c5da28baf6bf482822da0b3257cd8dc96
Moulimanju95 <mouli.manju1995@gmail.com>
2024-12-03 19:42:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,18 +24,60 @@ document
   });
 
 // JavaScript for Hamburger Menu Toggle
-document.getElementById("hamburger").addEventListener("click", function () {
-  const navLinks = document.getElementById("nav-links");
-  navLinks.classList.toggle("hidden");
+const body = document.querySelector("body"),
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  searchToggle = document.querySelector(".searchToggle"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  siderbarClose = document.querySelector(".siderbarClose");
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark");
+}
+// js code to toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("dark");
+  // js code to keep user selected mode even page refresh or file reopen
+  if (!body.classList.contains("dark")) {
+    localStorage.setItem("mode", "light-mode");
+  } else {
+    localStorage.setItem("mode", "dark-mode");
+  }
+});
+// js code to toggle search box
+searchToggle.addEventListener("click", () => {
+  searchToggle.classList.toggle("active");
 });
 
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+  if (
+    !clickedElm.classList.contains("sidebarOpen") &&
+    !clickedElm.classList.contains("menu")
+  ) {
+    nav.classList.remove("active");
+  }
+});
 
 // Dynamically Populate Skills List
-const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "MongoDB",
+  "SQL",
+];
 
-const skillsList = document.getElementById('skills-list');
-skills.forEach(skill => {
-    const listItem = document.createElement('li');
-    listItem.textContent = skill;
-    skillsList.appendChild(listItem);
+const skillsList = document.getElementById("skills-list");
+skills.forEach((skill) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = skill;
+  skillsList.appendChild(listItem);
 });
